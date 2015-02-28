d5f80794c98f770946efb5b23aaef823273b2bf7
James Lin <jl3782@columbia.edu>
2015-02-28 04:46:32 -0500

    overhaul(*): have generally working example now

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,107 +1,50 @@
-angular.module('mychat.controllers', [])
+var count = 0;
 
-.controller('LoginCtrl', function ($scope, $ionicModal, $state, $firebaseAuth, $ionicLoading, $rootScope) {
-    //console.log('Login Controller Initialized');
+angular.module('starter.controllers', [])
 
-    var ref = new Firebase($scope.firebaseUrl);
-    var auth = $firebaseAuth(ref);
+.controller('DashCtrl', function($scope, $firebase) {
+  var ref = new Firebase("https://fiery-fire-2843.firebaseio.com/users");
+  var sync = $firebase(ref);
 
-    $ionicModal.fromTemplateUrl('templates/signup.html', {
-        scope: $scope
-    }).then(function (modal) {
-        $scope.modal = modal;
-    });
+  $scope.users = sync.$asArray();
 
-    $scope.createUser = function (user) {
-        console.log("Create User Function called");
-        if (user && user.email && user.password && user.displayname) {
-            $ionicLoading.show({
-                template: 'Signing Up...'
-            });
+  $scope.addUser = function(username, description) {
+    $scope.users.$add({username: username, description: description, userID: count});
+    count++;
+  }
 
-            auth.$createUser({
-                email: user.email,
-                password: user.password
-            }).then(function (userData) {
-                alert("User created successfully!");
-                ref.child("users").child(userData.uid).set({
-                    email: user.email,
-                    displayName: user.displayname
-                });
-                $ionicLoading.hide();
-                $scope.modal.hide();
-            }).catch(function (error) {
-                alert("Error: " + error);
-                $ionicLoading.hide();
-            });
-        } else
-            alert("Please fill all details");
-    }
-
-    $scope.signIn = function (user) {
-
-        if (user && user.email && user.pwdForLogin) {
-            $ionicLoading.show({
-                template: 'Signing In...'
-            });
-            auth.$authWithPassword({
-                email: user.email,
-                password: user.pwdForLogin
-            }).then(function (authData) {
-                console.log("Logged in as:" + authData.uid);
-                ref.child("users").child(authData.uid).once('value', function (snapshot) {
-                    var val = snapshot.val();
-                    // To Update AngularJS $scope either use $apply or $timeout
-                    $scope.$apply(function () {
-                        $rootScope.displayName = val;
-                    });
-                });
-                $ionicLoading.hide();
-                $state.go('tab.rooms');
-            }).catch(function (error) {
-                alert("Authentication failed:" + error.message);
-                $ionicLoading.hide();
-            });
-        } else
-            alert("Please enter email and password both");
-    }
 })
 
-.controller('ChatCtrl', function ($scope, Chats, $state) {
-    //console.log("Chat Controller initialized");
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   }
+// })
 
-    $scope.IM = {
-        textMessage: ""
-    };
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
 
-    Chats.selectRoom($state.params.roomId);
+.controller('FriendsCtrl', function($scope, $firebase) {
+  var ref = new Firebase("https://fiery-fire-2843.firebaseio.com/users");
+  var sync = $firebase(ref);
 
-    var roomName = Chats.getSelectedRoomName();
-
-    // Fetching Chat Records only if a Room is Selected
-    if (roomName) {
-        $scope.roomName = " - " + roomName;
-        $scope.chats = Chats.all();
-    }
-
-    $scope.sendMessage = function (msg) {
-        console.log(msg);
-        Chats.send($scope.displayName, msg);
-        $scope.IM.textMessage = "";
-    }
-
-    $scope.remove = function (chat) {
-        Chats.remove(chat);
-    }
+  $scope.users = sync.$asArray();
 })
 
-.controller('RoomsCtrl', function ($scope, Rooms, Chats, $state) {
-    //console.log("Rooms Controller initialized");
-    $scope.rooms = Rooms.all();
+.controller('FriendDetailCtrl', function($scope, $firebase, $stateParams) {
 
-    $scope.openChatRoom = function (roomId) {
-        $state.go('tab.chat', {
-            roomId: roomId
-        });
-    }
+  var ref = new Firebase("https://fiery-fire-2843.firebaseio.com/users");
+  var userList = $firebase(ref).$asArray();
+
+  userList.$loaded().then(function(userList) {
+    $scope.user = userList[$stateParams.userID];
+  });
 });
+
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// });
--- www/js/app.js
+++ www/js/app.js
@@ -1,129 +1,102 @@
-// MyChat App - Ionic & Firebase Demo
+// Ionic Starter App
 
-var firebaseUrl = "https://fiery-fire-2843.firebaseio.com";
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
 
-function onDeviceReady() {
-    angular.bootstrap(document, ["mychat"]);
-}
-//console.log("binding device ready");
-// Registering onDeviceReady callback with deviceready event
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// 'mychat.services' is found in services.js
-// 'mychat.controllers' is found in controllers.js
-angular.module('mychat', ['ionic', 'firebase', 'angularMoment', 'mychat.controllers', 'mychat.services'])
-
-.run(function ($ionicPlatform, $rootScope, $location, Auth, $ionicLoading) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-        // To Resolve Bug
-        ionic.Platform.fullScreen();
-
-        $rootScope.firebaseUrl = firebaseUrl;
-        $rootScope.displayName = null;
-
-        Auth.$onAuth(function (authData) {
-            if (authData) {
-                console.log("Logged in as:", authData.uid);
-            } else {
-                console.log("Logged out");
-                $ionicLoading.hide();
-                $location.path('/login');
-            }
-        });
-
-        $rootScope.logout = function () {
-            console.log("Logging out from the app");
-            $ionicLoading.show({
-                template: 'Logging Out...'
-            });
-            Auth.$unauth();
-        }
-
-
-        $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
-            // We can catch the error thrown when the $requireAuth promise is rejected
-            // and redirect the user back to the home page
-            if (error === "AUTH_REQUIRED") {
-                $location.path("/login");
-            }
-        });
-    });
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
-    console.log("setting config");
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
+.config(function($stateProvider, $urlRouterProvider) {
 
-    // State to represent Login View
-    .state('login', {
-        url: "/login",
-        templateUrl: "templates/login.html",
-        controller: 'LoginCtrl',
-        resolve: {
-            // controller will not be loaded until $waitForAuth resolves
-            // Auth refers to our $firebaseAuth wrapper in the example above
-            "currentAuth": ["Auth",
-                function (Auth) {
-                    // $waitForAuth returns a promise so the resolve waits for it to complete
-                    return Auth.$waitForAuth();
-        }]
-        }
-    })
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-
-    // setup an abstract state for the tabs directive
+  // setup an abstract state for the tabs directive
     .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html",
-        resolve: {
-            // controller will not be loaded until $requireAuth resolves
-            // Auth refers to our $firebaseAuth wrapper in the example above
-            "currentAuth": ["Auth",
-                function (Auth) {
-                    // $requireAuth returns a promise so the resolve waits for it to complete
-                    // If the promise is rejected, it will throw a $stateChangeError (see above)
-                    return Auth.$requireAuth();
-      }]
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  // .state('tab.chats', {
+  //     url: '/chats',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/tab-chats.html',
+  //         controller: 'ChatsCtrl'
+  //       }
+  //     }
+  //   })
+  //   .state('tab.chat-detail', {
+  //     url: '/chats/:chatId',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/chat-detail.html',
+  //         controller: 'ChatDetailCtrl'
+  //       }
+  //     }
+  //   })
+
+  .state('tab.friends', {
+      url: '/friends',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/tab-friends.html',
+          controller: 'FriendsCtrl'
         }
+      }
     })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.rooms', {
-        url: '/rooms',
-        views: {
-            'tab-rooms': {
-                templateUrl: 'templates/tab-rooms.html',
-                controller: 'RoomsCtrl'
-            }
+    .state('tab.friend-detail', {
+      url: '/friend/:userID',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/friend-detail.html',
+          controller: 'FriendDetailCtrl'
         }
-    })
+      }
+    });
 
-    .state('tab.chat', {
-        url: '/chat/:roomId',
-        views: {
-            'tab-chat': {
-                templateUrl: 'templates/tab-chat.html',
-                controller: 'ChatCtrl'
-            }
-        }
-    })
+  // .state('tab.account', {
+  //   url: '/account',
+  //   views: {
+  //     'tab-account': {
+  //       templateUrl: 'templates/tab-account.html',
+  //       controller: 'AccountCtrl'
+  //     }
+  //   }
+  // });
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
 
 });
